670dfdb4b7fd12d163b59e39dd08882ff7785077
Arundhati <arundhati123@gmail.com>
2025-12-04 15:13:26 +0530

    tf

---
+++ terraform/variableblock/outputblock/main.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "webserver" {
+    ingress = {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
