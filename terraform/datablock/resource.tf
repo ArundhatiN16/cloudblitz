6ac63881048d3ba684e9f7a0500326738d9bd8a1
Arundhati <arundhati123@gmail.com>
2025-12-05 19:24:46 +0530

    tf

--- terraform/datablock/resource.tf
+++ terraform/datablock/resource.tf
@@ -1,6 +1,21 @@
-resource "aws_instance" "demo_tf" {
-  instance_type          = "t3.micro"
-  ami                    = "ami-0d176f79571d18a8f"
-  key_name               = "8-08"
-  vpc_security_group_ids = ["sg-0c2bf931f8a6e8abb"]
+resource "aws_instance" "webserver" {
+    instance_type = var.webserver_instance_type
+    ami = var.webserver_ami
+    key_name = var.webserver_key_name
+    vpc_security_group_ids = [var.webserver_vpc_security_group_ids , aws_security_group.webserver.id , data.aws_security_group.aws_sg.id ] 
+    disable_api_termination = var.webserver_disable_api_termination
+}
+resource "aws_security_group" "webserver" {
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
