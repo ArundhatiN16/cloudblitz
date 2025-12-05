5f1fe4f166f9c077e91833aa6212bd8ca96ffc29
Arundhati <arundhati123@gmail.com>
2025-12-05 15:25:20 +0530

    tf

--- datablock/resource.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "demo_tf" {
-  instance_type          = "t3.micro"
-  ami                    = "ami-0d176f79571d18a8f"
-  key_name               = "8-08"
-  vpc_security_group_ids = ["sg-0c2bf931f8a6e8abb"]
-}
