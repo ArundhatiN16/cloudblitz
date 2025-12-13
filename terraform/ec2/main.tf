07c6853429651adab2267a1a1a9f4835d1f18ebc
Arundhati <arundhati123@gmail.com>
2025-12-13 13:33:47 +0530

    tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
     instance_type = var.webserver_instance_type
     key_name = var.webserver_key_name
-    vpc_security_group_ids = var.webserver_vpc_security_group_ids
+    vpc_security_group_ids = [var.webserver_vpc_security_group_id]
     ami = var.webserver_ami
 }
--- terraform/ec2/variable.tf
+++ terraform/ec2/variable.tf
@@ -7,6 +7,6 @@ variable "webserver_instance_type" {
 variable "webserver_key_name" {
     default = "8-08"     
 }
-variable "webserver_vpc_security_group_ids" {
-    default = [ "sg-0090938ab0d114e84" ]
+variable "webserver_vpc_security_group_id" {
+    default = "sg-0090938ab0d114e84"
 }
