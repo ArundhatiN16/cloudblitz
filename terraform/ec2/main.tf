921f6be9916c229c7c87febff61aaa820a2e7b41
Arundhati <arundhati123@gmail.com>
2025-12-13 14:41:57 +0530

    tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -3,12 +3,23 @@ resource "aws_instance" "ec2" {
     key_name = var.webserver_key_name
     vpc_security_group_ids = [var.webserver_vpc_security_group_ids]
     ami = var.webserver_ami
-        user_data = <<-EOF
-                    #!/bin/bash
-                    sudo apt update -y
-                    sudo apt install -y nginx
-                    sudo systemctl enable nginx
-                    sudo systemctl start nginx
-                    echo "<h1>Hello Terraform</h1>" > /var/www/html/index.html
-                    EOF
+user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install -y apache2
+              systemctl start apache2
+              cat <<HTML > /var/www/html/index.html
+              <!DOCTYPE html>
+              <html>
+              <head>
+                  <title>Terraform EC2 Web Server</title>
+              </head>
+              <body style="background-color: #f4f4f4; text-align: center;">
+                  <h1>🚀 Hello from Terraform User Data</h1>
+                  <p>EC2 instance successfully launched using Terraform</p>
+                  <p><strong>Environment:</strong> Dev</p>
+              </body>
+              </html>
+              HTML
+              EOF
 }
