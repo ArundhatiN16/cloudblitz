021ffec4b235780677769b6049317f1e3c32247b
Arundhati <arundhati123@gmail.com>
2025-12-13 14:21:34 +0530

    tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -3,13 +3,10 @@ resource "aws_instance" "ec2" {
     key_name = var.webserver_key_name
     vpc_security_group_ids = [var.webserver_vpc_security_group_ids]
     ami = var.webserver_ami
-
 user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update -y
-    sudo apt install nginx -y
-    sudo systemctl enable nginx
-    sudo systemctl start nginx
-    echo "Nginx Page" > /var/www/html/index.html
-  EOF
+              #!/bin/bash
+              sudo apt install -y nginx
+              systemctl start nginx
+              echo "<h1>Hello from Terraform</h1>" > /var/www/html/index.html
+              EOF
 }
